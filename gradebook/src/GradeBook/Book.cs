3237c37a69b217d9fec4928ccc52725c182ce7e8
Brian Hogan <bkhogan2@gmail.com>
2019-09-03 08:15:49 -0700

    Working with Classes and Objects

---
+++ gradebook/src/GradeBook/Book.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook 
+{
+    class Book 
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void AddGrade(double grade) 
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics() {
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            var result = 0.0;
+
+            foreach (var number in grades) {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+
+            var average = result / grades.Count;
+
+            Console.WriteLine($"The average grade is {average:N1}");
+            Console.WriteLine($"The highest grade is {highGrade:N1}");
+            Console.WriteLine($"The lowest grade is {lowGrade:N1}");
+        }
+
+        private List<double> grades;
+        private string name;
+    }
+}
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -7,30 +7,11 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
-            grades.Add(56.1);
-            
-
-            var result = 0.0;
-
-            foreach (var number in grades) {
-                result += number;
-            }
-
-            var average = result / grades.Count;
-
-            System.Console.WriteLine($"The average grade is {average}");
-
-            Console.WriteLine(average);
-
-            if(args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]}!");
-            } 
-            else
-            {
-                Console.WriteLine("Hello!");
-            }
+            var book = new Book("Brian's Gradebook");
+            book.AddGrade(89.1);
+            book.AddGrade(90.1);
+            book.AddGrade(99.9);
+            book.ShowStatistics();
         }
     }
 }
