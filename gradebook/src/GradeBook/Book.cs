729a2a8a0eb49885261624908dc7f1aa8066b911
Brian Hogan <bkhogan2@gmail.com>
2019-09-03 13:54:23 -0700

    Controlling the Flow of Execution

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -10,14 +10,32 @@ namespace GradeBook
             grades = new List<double>();
             Name = name;
         }
-
+        public void AddLetterGrade(char letter) {
+            switch(letter) {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
         public void AddGrade(double grade) 
         {
             if (grade <= 100 && grade >= 0) {
                 grades.Add(grade);
             }
             else {
-                System.Console.WriteLine("Invalid Value");
+                throw new ArgumentException($"Invalid {nameof(grade)}");
             }   
         }
 
@@ -27,12 +45,35 @@ namespace GradeBook
             result.High = double.MinValue;
             result.Low = double.MaxValue;
 
-            foreach (var grade in grades) {
-                result.High = Math.Max(grade, result.High);
-                result.Low = Math.Min(grade, result.Low);
-                result.Average += grade;
+            for (var index = 0; index < grades.Count; index++) {
+                result.High = Math.Max(grades[index], result.High);
+                result.Low = Math.Min(grades[index], result.Low);
+                result.Average += grades[index];
             }
+
             result.Average /= grades.Count;
+
+            switch(result.Average) {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
 
             return result;
         }
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -8,16 +8,37 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Brian's Gradebook");
-            book.AddGrade(89.1);
-            book.AddGrade(90.1);
-            book.AddGrade(99.9);
-            book.GetStatistics();
+            // .. get user input
 
+            while (true){
+                Console.WriteLine("Please enter the next grade.");
+                var input = Console.ReadLine();
+                if (input == "exit") {
+                    break;
+                }
+
+                try {
+                    var grade = double.Parse(input);
+
+                    book.AddGrade(grade);
+                }
+                catch(ArgumentException ex) {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex) {
+                    System.Console.WriteLine(ex.Message);
+                }
+                finally{
+                    Console.WriteLine("**");
+                }
+            }
+            
             var stats = book.GetStatistics();
 
             Console.WriteLine($"The average grade is {stats.Average}");
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
         }
     }
 }
